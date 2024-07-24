b0837e03a1cb7124218fc1ced07ece7ef73b92df
Santiago Betancur Duque <santiagobetancurd.contratista@comfama.com.co>
2024-07-23 22:41:37 -0500

    feat: initialized both state handlers projects

---
+++ react-api-context/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div >
+      <h1>Api Context</h1>
+      <p>State handler</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-redux/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Redux</h1>
+      <p>state handler</p>
+    </div>
+  );
+}
+
+export default App;
